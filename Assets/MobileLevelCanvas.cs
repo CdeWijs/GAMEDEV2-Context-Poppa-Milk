a608191923e330a4429057737457b8899c467716
Celine de Wijs <31764854+CdeWijs@users.noreply.github.com>
2018-03-27 09:37:49 +0200

    Mobile Level Canvas

---
+++ Assets/MobileLevelCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileLevelCanvas : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+
+        if (InputMobile.instance != null)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
